6c6639a229fe6ab6d0f867ebfdaaa4461f4f0bc5
Jeremy-Potratz <jpotrat@purdue.edu>
2020-05-19 17:15:23 -0500

    Deploy

---
+++ src/components/Github.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Github">
+    <p>
+      Github! Here is the code for this website LINK
+    </p>
+    <a href="https://www.github.com/Jeremy-Potratz" target="_blank">Want to see my Github?</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Github',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
